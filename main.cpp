4ef1176f3bb9484a76cc5861e35f74945173f296
Vietphogn <100075632+Vietphogn@users.noreply.github.com>
2025-02-03 22:51:31 +0700

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,53 @@
+#include "raylib.h"
+
+class Player {
+public:
+    Vector2 position = {400, 760};
+    Texture2D texture;
+
+    Player() {
+        Image image = LoadImage("assets/player.png");
+        texture = LoadTextureFromImage(image);
+        UnloadImage(image);
+    }
+
+    ~Player() {
+        UnloadTexture(texture);
+    }
+
+    void Draw() {
+        DrawTexture(texture, position.x, position.y, WHITE);
+    }
+};
+
+class Enemy {
+public:
+    
+};
+
+int main() {
+    InitWindow(800, 800, "Space Invader (nah phogn'd win)");
+    SetTargetFPS(60);Image image = LoadImage("assets/player.png");
+
+    Player player = Player();
+
+
+    while (WindowShouldClose() == false) {
+        BeginDrawing();
+
+        if (IsKeyDown(KEY_LEFT)) {
+            player.position.x -= 3;
+        }
+        if (IsKeyDown(KEY_RIGHT)) {
+            player.position.x += 3;
+        }
+
+        ClearBackground(BLACK);
+        player.Draw();
+
+        EndDrawing();
+    }
+
+    CloseWindow();
+    return 0;
+}
